2b2cd35b19babff85b55bbcfc063abceadb23b83
George Lugo <machinelearner2334@gmail.com>
2025-06-18 23:54:14 +0000

    feat: added configurations

--- example/next-storefront.config.js
+++ example/next-storefront.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sources: [
     require('@next-storefront/shopify'),
-    require('@next-storefront/json'),
+    // require('@next-storefront/json'), // Commented out - using only Shopify now
   ],
 }
